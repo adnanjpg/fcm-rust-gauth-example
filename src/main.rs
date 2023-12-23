9f0ee85fd20e79ba6d7bfcf4c50525fe8099fa37
adnanjpg <adnanfahedhr@gmail.com>
2023-12-23 10:58:47 +0300

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+mod notification_service;
+
+#[tokio::main]
+async fn main() {
+    // TODO: fill this
+    let device_id = "";
+
+    let res = notification_service::send_notification_to(&device_id).await;
+
+    match res {
+        Ok(_) => println!("notification sent"),
+        Err(err) => println!("error: {}", err),
+    }
 }
